48c22366910894e732a9fac1bfeee43d3b0825ed
Larson Ost <larsonost@outlook.com>
2023-08-08 14:45:36 -0400

    a5 REST su23

--- users/users-controller.js
+++ users/users-controller.js
@@ -1,5 +1,6 @@
-import people from './users.js'
-let users = people
+import people from "./users.js";
+let users = people;
+
 const UserController = (app) => {
     app.get('/api/users', findUsers)
     app.get('/api/users/:uid', findUserById);
@@ -11,33 +12,29 @@ const updateUser = (req, res) => {
     const userId = req.params['uid'];
     const updates = req.body;
     users = users.map((usr) =>
-      usr._id === userId ?
-        {...usr, ...updates} :
-        usr
+        usr._id === userId ?
+            { ...usr, ...updates } :
+            usr
     );
     res.sendStatus(200);
-   }
-   
+}
 const deleteUser = (req, res) => {
     const userId = req.params['uid'];
     users = users.filter(usr => usr._id !== userId);
     res.sendStatus(200);
-  }
-  
+}
 const createUser = (req, res) => {
     const newUser = req.body;
     newUser._id = (new Date()).getTime() + '';
     users.push(newUser);
     res.json(newUser);
-  }
-  
+}
 const findUserById = (req, res) => {
     const userId = req.params.uid;
     const user = users
         .find(u => u._id === userId);
     res.json(user);
 }
-
 const findUsers = (req, res) => {
     const type = req.query.type
     if (type) {
@@ -46,7 +43,7 @@ const findUsers = (req, res) => {
         res.json(usersOfType)
         return
     }
-
     res.json(users)
 }
+
 export default UserController
--- users/users.js
+++ users/users.js
@@ -1,6 +1,34 @@
 export default [
-    { "username": "alice",   "type": "FACULTY", "_id": "123" },
-    { "username": "bob",     "type": "STUDENT", "_id": "234" },
-    { "username": "charlie", "type": "FACULTY", "_id": "345" }
-  ]
-  
+  {
+    username: "alice",
+    password: "1234",
+    firstName: "Alice",
+    lastName: "Wonderland",
+    type: "FACULTY",
+    _id: "1"
+  },
+  {
+    username: "bob",
+    firstName: "Bob",
+    password: "1234",
+    lastName: "Marley",
+    type: "STUDENT",
+    _id: "2"
+  },
+  {
+    username: "charlie",
+    type: "FACULTY",
+    _id: "3",
+    password: "1234",
+    firstName: "Charlie",
+    lastName: "Chaplin"
+  },
+  {
+    username: "dan",
+    type: "STUDENT",
+    _id: "4",
+    password: "1234",
+    firstName: "Dan",
+    lastName: "Craig"
+  }
+]
